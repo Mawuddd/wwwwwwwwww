e1b2e3105a5428343f7774d54e56d63f13000627
nuister-nk <89286395+nuister-nk@users.noreply.github.com>
2022-02-21 22:16:10 +0800

    Update checkin.py

--- checkin.py
+++ checkin.py
@@ -7,11 +7,10 @@ sever = os.environ["SERVE"]
 sckey = os.environ["SCKEY"]
 
 # 填入glados账号对应cookie
-cookie1 = os.environ["COOKIE1"] # 2226840339@qq.com
-cookie2 = os.environ["COOKIE2"] # 20171376006@nuist.edu.cn
-cookie3 = os.environ["COOKIE3"] # 2315230220@qq.com
-cookie4 = os.environ["COOKIE4"] # 1713863719@qq.com
-cookie5 = os.environ["COOKIE5"] # 3494620766@qq.com
+cookie1 = os.environ["COOKIE1"] # 20211201028@nuist.edu.cn
+cookie2 = os.environ["COOKIE2"] # 2021nuister@gmail.com
+cookie3 = os.environ["COOKIE3"] # 2226840339@qq.com
+
 
 
 
@@ -19,10 +18,8 @@ def start():
     url= "https://glados.rocks/api/user/checkin"
     url2= "https://glados.rocks/api/user/status"
     referer = 'https://glados.rocks/console/checkin'
-    #checkin = requests.post(url,headers={'cookie': cookie ,'referer': referer })
-    #state =  requests.get(url2,headers={'cookie': cookie ,'referer': referer})
     origin = "https://glados.rocks"
-    useragent = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/96.0.4664.45 Safari/537.36 Edg/96.0.1054.29"
+    useragent = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/98.0.4758.102 Safari/537.36 Edg/98.0.1108.56"
     payload={
         'token': 'glados_network'
     }
@@ -35,11 +32,6 @@ def start():
     checkin3 = requests.post(url,headers={'cookie': cookie3 ,'referer': referer,'origin':origin,'user-agent':useragent,'content-type':'application/json;charset=UTF-8'},data=json.dumps(payload))
     state3 =  requests.get(url2,headers={'cookie': cookie3 ,'referer': referer,'origin':origin,'user-agent':useragent})
     
-    checkin4 = requests.post(url,headers={'cookie': cookie4 ,'referer': referer,'origin':origin,'user-agent':useragent,'content-type':'application/json;charset=UTF-8'},data=json.dumps(payload))
-    state4 =  requests.get(url2,headers={'cookie': cookie4 ,'referer': referer,'origin':origin,'user-agent':useragent})
-    
-    checkin5 = requests.post(url,headers={'cookie': cookie5 ,'referer': referer,'origin':origin,'user-agent':useragent,'content-type':'application/json;charset=UTF-8'},data=json.dumps(payload))
-    state5 =  requests.get(url2,headers={'cookie': cookie5 ,'referer': referer,'origin':origin,'user-agent':useragent})
     
 
     if 'message' in checkin1.text:
@@ -48,9 +40,9 @@ def start():
         time = time.split('.')[0]
         print(time)
         if sever == 'on':
-            requests.get('https://sctapi.ftqq.com/' + sckey + '.send?text='+mess+'，222账号剩余'+time+'天')
+            requests.get('https://sctapi.ftqq.com/' + sckey + '.send?text='+mess+'，2021edu'+time+'天')
     else:
-        requests.get('https://sctapi.ftqq.com/' + sckey + '.send?text=222的cookie过期！请更新COOKIE1')
+        requests.get('https://sctapi.ftqq.com/' + sckey + '.send?text=2021edu需更新COOKIE1')
     #--------------------------------------------------------------------------------------------------------#  
     if 'message' in checkin2.text:
         mess = checkin2.json()['message']
@@ -58,9 +50,9 @@ def start():
         time = time.split('.')[0]
         print(time)
         if sever == 'on':
-            requests.get('https://sctapi.ftqq.com/' + sckey + '.send?text='+mess+'，nuist账号剩余'+time+'天')
+            requests.get('https://sctapi.ftqq.com/' + sckey + '.send?text='+mess+'，2021谷歌'+time+'天')
     else:
-        requests.get('https://sctapi.ftqq.com/' + sckey + '.send?text=nuist邮箱的cookie过期！请更新COOKIE2')
+        requests.get('https://sctapi.ftqq.com/' + sckey + '.send?text=2021谷歌需更新COOKIE2')
      #--------------------------------------------------------------------------------------------------------#   
     if 'message' in checkin3.text:
         mess = checkin3.json()['message']
@@ -68,29 +60,10 @@ def start():
         time = time.split('.')[0]
         print(time)
         if sever == 'on':
-            requests.get('https://sctapi.ftqq.com/' + sckey + '.send?text='+mess+'，231账号剩余'+time+'天')
+            requests.get('https://sctapi.ftqq.com/' + sckey + '.send?text='+mess+'，222QQ'+time+'天')
     else:
-        requests.get('https://sctapi.ftqq.com/' + sckey + '.send?text=231邮箱的cookie过期！请更新COOKIE3')
+        requests.get('https://sctapi.ftqq.com/' + sckey + '.send?text=222QQ需更新COOKIE3')
      #--------------------------------------------------------------------------------------------------------#   
-    if 'message' in checkin4.text:
-        mess = checkin4.json()['message']
-        time = state4.json()['data']['leftDays']
-        time = time.split('.')[0]
-        print(time)
-        if sever == 'on':
-            requests.get('https://sctapi.ftqq.com/' + sckey + '.send?text='+mess+'，171账号剩余'+time+'天')
-    else:
-        requests.get('https://sctapi.ftqq.com/' + sckey + '.send?text=171邮箱的cookie过期！请更新COOKIE4')
-     #--------------------------------------------------------------------------------------------------------#  
-    if 'message' in checkin5.text:
-        mess = checkin5.json()['message']
-        time = state5.json()['data']['leftDays']
-        time = time.split('.')[0]
-        print(time)
-        if sever == 'on':
-            requests.get('https://sctapi.ftqq.com/' + sckey + '.send?text='+mess+'，349账号剩余'+time+'天')
-    else:
-        requests.get('https://sctapi.ftqq.com/' + sckey + '.send?text=349邮箱的cookie过期！请更新COOKIE5')
 
 
 def main_handler(event, context):
