77e8e62eb7bd3bf910269afc6a18ad77eea4f594
nuister-nk <89286395+nuister-nk@users.noreply.github.com>
2022-05-13 11:07:57 +0800

    Update checkin.py

--- checkin.py
+++ checkin.py
@@ -7,10 +7,8 @@ sever = os.environ["SERVE"]
 sckey = os.environ["SCKEY"]
 
 # 填入glados账号对应cookie
-cookie1 = os.environ["COOKIE1"] # 20211201028@nuist.edu.cn
-cookie2 = os.environ["COOKIE2"] # 2021nuister@gmail.com
-cookie3 = os.environ["COOKIE3"] # 2226840339@qq.com
-
+COOKIES = os.environ["COOKIES"]
+cookies=COOKIES.split('&&')
 
 
 
@@ -23,46 +21,19 @@ def start():
     payload={
         'token': 'glados.network'
     }
-    checkin1 = requests.post(url,headers={'cookie': cookie1 ,'referer': referer,'origin':origin,'user-agent':useragent,'content-type':'application/json;charset=UTF-8'},data=json.dumps(payload))
-    state1 =  requests.get(url2,headers={'cookie': cookie1 ,'referer': referer,'origin':origin,'user-agent':useragent})
-    
-    checkin2 = requests.post(url,headers={'cookie': cookie2 ,'referer': referer,'origin':origin,'user-agent':useragent,'content-type':'application/json;charset=UTF-8'},data=json.dumps(payload))
-    state2 =  requests.get(url2,headers={'cookie': cookie2 ,'referer': referer,'origin':origin,'user-agent':useragent})
-    
-    checkin3 = requests.post(url,headers={'cookie': cookie3 ,'referer': referer,'origin':origin,'user-agent':useragent,'content-type':'application/json;charset=UTF-8'},data=json.dumps(payload))
-    state3 =  requests.get(url2,headers={'cookie': cookie3 ,'referer': referer,'origin':origin,'user-agent':useragent})
-    
-    
-
-    time1 = state1.json()['data']['leftDays']
-    time1 = time1.split('.')[0]
-    email1 = state1.json()['data']['email']
-    if 'message' in checkin1.text:
-        mess = checkin1.json()['message']
-        if sever == 'on':
-            requests.get('http://www.pushplus.plus/send?token=' + sckey + '&title='+mess+'&content='+email1+' 剩余'+time1+'天')
-    else:
-        requests.get('http://www.pushplus.plus/send?token=' + sckey + '&content='+email1+'更新cookie')
-    #--------------------------------------------------------------------------------------------------------# 
-    time2 = state2.json()['data']['leftDays']
-    time2 = time2.split('.')[0]
-    email2 = state2.json()['data']['email']
-    if 'message' in checkin2.text:
-        mess = checkin2.json()['message']
-        if sever == 'on':
-            requests.get('http://www.pushplus.plus/send?token=' + sckey + '&title='+mess+'&content='+email2+' 剩余'+time2+'天')
-    else:
-        requests.get('http://www.pushplus.plus/send?token=' + sckey + '&content='+email2+'更新cookie')
-     #--------------------------------------------------------------------------------------------------------#   
-    time3 = state3.json()['data']['leftDays']
-    time3 = time3.split('.')[0]
-    email3 = state3.json()['data']['email']
-    if 'message' in checkin3.text:
-        mess = checkin3.json()['message']
-        if sever == 'on':
-            requests.get('http://www.pushplus.plus/send?token=' + sckey + '&title='+mess+'&content='+email3+' 剩余'+time3+'天')
-    else:
-        requests.get('http://www.pushplus.plus/send?token=' + sckey + '&content='+email3+'更新cookie')
+    for cookie in cookies:
+        checkin = requests.post(url,headers={'cookie': cookie ,'referer': referer,'origin':origin,'user-agent':useragent,'content-type':'application/json;charset=UTF-8'},data=json.dumps(payload))
+        state =  requests.get(url2,headers={'cookie': cookie ,'referer': referer,'origin':origin,'user-agent':useragent})
+    #--------------------------------------------------------------------------------------------------------#  
+        time = state.json()['data']['leftDays']
+        time = time.split('.')[0]
+        email = state.json()['data']['email']
+        if 'message' in checkin.text:
+            mess = checkin.json()['message']
+            if sever == 'on':
+                requests.get('http://www.pushplus.plus/send?token=' + sckey + '&title='+mess+'&content='+email+' 剩余'+time+'天')
+        else:
+            requests.get('http://www.pushplus.plus/send?token=' + sckey + '&content='+email+'更新cookie')
      #--------------------------------------------------------------------------------------------------------#   
 
 
